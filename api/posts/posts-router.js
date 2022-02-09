c4e936044c5f1ce192894eccf2514ebcffc04da1
abdullahi0520 <90243393+abdullahi0520@users.noreply.github.com>
2022-02-08 18:23:57 -0600

    first commit

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,31 @@
 // implement your posts router here
+const express = require('express')
+const post = require('./posts-model')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    post.find()
+    .then(found => {
+        res.json(found)
+    })
+    .catch(() => {
+        res.status(500).json({message:"The posts information could not be retrieved"})
+    })
+})
+router.get('/:id', (req, res) => {
+
+})
+router.post('/', (req, res) => {
+
+})
+router.delete('/:id', (req, res) => {
+
+})
+router.put('/:id', (req, res) => {
+
+})
+router.get('/:id/messages', (req, res) => {
+
+})
+
+module.exports= router
